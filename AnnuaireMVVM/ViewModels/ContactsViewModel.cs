b8baeaef7206e9d0e2f1308ac866b2b0ecc93632
ihab abadi <ihab@utopios.net>
2021-03-08 15:07:47 +0100

    correction bouton supprimer

--- AnnuaireMVVM/ViewModels/ContactsViewModel.cs
+++ AnnuaireMVVM/ViewModels/ContactsViewModel.cs
@@ -14,7 +14,7 @@ namespace AnnuaireMVVM.ViewModels
     {
         private Contact contact;
 
-        public Contact Contact { get => contact; set { contact = value; RaiseAllChanged(); } }
+        public Contact Contact { get => contact; set { contact = value; if(value != null) RaiseAllChanged(); } }
 
         public string Nom { get => Contact.Nom; set { Contact.Nom = value; RaisePropertyChanged(); } }
         public string Prenom { get => Contact.Prenom; set { Contact.Prenom = value; RaisePropertyChanged();} }
@@ -22,14 +22,16 @@ namespace AnnuaireMVVM.ViewModels
 
         public ICommand ConfirmCommand { get; set; }
 
+        public ICommand DeleteCommand { get; set; }
+
         public ObservableCollection<Contact> Contacts { get; set; }
 
         public ContactsViewModel()
         {
             Contact = new Contact();
             ConfirmCommand = new RelayCommand(ActionConfirmCommand);
+            DeleteCommand = new RelayCommand(ActionDeleteCommand);
             Contacts = new ObservableCollection<Contact>(Contact.GetContacts());
-
         }
 
         public void ActionConfirmCommand()
@@ -50,6 +52,18 @@ namespace AnnuaireMVVM.ViewModels
             else
             {
                 MessageBox.Show("Erreur");
+            }
+        }
+
+        public void ActionDeleteCommand()
+        {
+            if(Contact.Id > 0)
+            {
+                if (Contact.Delete())
+                {
+                    Contacts.Remove(Contact);
+                    Contact = new Contact();
+                }
             }
         }
 
