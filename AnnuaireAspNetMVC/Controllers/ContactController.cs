198248c750cb4e754ee69ce6db21f8313500f8c3
ihab abadi <ihab@utopios.net>
2021-03-10 15:24:41 +0100

    correction annuaire viewdata et viewbag

--- AnnuaireAspNetMVC/Controllers/ContactController.cs
+++ AnnuaireAspNetMVC/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Annuaire.Classes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnnuaireAspNetMVC.Controllers
@@ -10,6 +11,8 @@ namespace AnnuaireAspNetMVC.Controllers
     {
         public IActionResult Index()
         {
+            //ViewData["contacts"] = Contact.GetContacts();
+            ViewBag.Contacts= Contact.GetContacts();
             return View();
         }
 
