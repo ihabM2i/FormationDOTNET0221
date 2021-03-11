f20ca518fa77e2aa3e9984774667b0e73527eb82
ihab abadi <ihab@utopios.net>
2021-03-11 12:03:04 +0100

    correction annuaire formulaire ajout contact

--- AnnuaireAspNetMVC/Controllers/ContactController.cs
+++ AnnuaireAspNetMVC/Controllers/ContactController.cs
@@ -9,16 +9,44 @@ namespace AnnuaireAspNetMVC.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string message)
         {
             //ViewData["contacts"] = Contact.GetContacts();
             ViewBag.Contacts= Contact.GetContacts();
+            ViewBag.Message = message;
             return View();
         }
 
         public IActionResult FormContact()
         {
             return View();
+        }
+
+        public IActionResult SubmitFormContact(string nom, string prenom, string telephone)
+        {
+            if(nom != null && prenom != null && telephone != null)
+            {
+                Contact contact = new Contact()
+                {
+                    Nom = nom,
+                    Prenom = prenom,
+                    Telephone = telephone
+                };
+                if(contact.Save())
+                {
+                    return RedirectToAction("Index", "Contact", new { message = "Contact ajouté"});
+                }
+                else
+                {
+                    ViewBag.MessageError = "Erreur d'insertion dans la base de données";
+                    return View("FormContact");
+                }
+            }
+            else
+            {
+                ViewBag.MessageError = "Merci de remplir la totalité des champs";
+                return View("FormContact");
+            }
         }
     }
 }
