201b3431a5048b5d33dfca6c84f12140f8e7528d
ihab abadi <ihab@utopios.net>
2021-03-15 14:52:42 +0100

    cours viewcomponent

---
+++ coursAspNetMVC/ViewComponents/MenuComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coursAspNetMVC.ViewComponents
+{
+    public class MenuComponent : ViewComponent
+    {
+        public MenuComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            List<string> liste = new List<string>() { "menu 1", "menu 2", "Menu 3", "menu4" };
+            return View(liste);
+        }
+    }
+}
