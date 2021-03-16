dd023edbb884523e36b65f1dbaf11b7f4758bb5a
ihab abadi <ihab@utopios.net>
2021-03-16 11:15:46 +0100

    cours upload multiple

--- coursAspNetMVC/Controllers/UploadController.cs
+++ coursAspNetMVC/Controllers/UploadController.cs
@@ -47,5 +47,35 @@ namespace coursAspNetMVC.Controllers
             a.Save();
             return RedirectToAction("Index");
         }
+
+        public IActionResult SubmitMultiUpload(string nom, IFormFile[] avatar)
+        {
+            //if(avatar.Length > 10000)
+            //{
+            //    throw new Exception("Large file");
+            //}
+            //Uploader l'image dans un dossier
+            //Chemin complet de sauvegarde de l'image
+            //string filePath = @"C:\Users\ihab\source\repos\CoursMCPDNETF\coursAspNetMVC\wwwroot\avatar-" + nom +"-"+avatar.FileName;
+            
+            foreach(IFormFile i in avatar)
+            {
+                string filePath = Path.Combine(_env.WebRootPath, "avatar", "avatar-" + nom + "-" + i.FileName);
+
+                //Créer un flux pour sauvegarder l'image => A l'aide de la classe FILE
+                Stream stream = System.IO.File.Create(filePath);
+                i.CopyTo(stream);
+                stream.Close();
+                //Enregistrer dans la base de données
+                string chemin = "avatar/avatar-" + nom + "-" + i.FileName;
+                Avatar a = new Avatar()
+                {
+                    Chemin = chemin
+                };
+                a.Save();
+            }
+            
+            return RedirectToAction("Index");
+        }
     }
 }
