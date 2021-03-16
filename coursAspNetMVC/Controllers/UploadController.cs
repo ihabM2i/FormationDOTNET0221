9c1a327bdea5c25ce251995c99f3cc1da8f4535d
ihab abadi <ihab@utopios.net>
2021-03-16 10:39:43 +0100

    cours upload

---
+++ coursAspNetMVC/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using coursAspNetMVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coursAspNetMVC.Controllers
+{
+    public class UploadController : Controller
+    {
+        private IWebHostEnvironment _env;
+        
+        public UploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View(Avatar.GetAll());
+        }
+
+        public IActionResult SubmitUpload(string nom, IFormFile avatar)
+        {
+            //if(avatar.Length > 10000)
+            //{
+            //    throw new Exception("Large file");
+            //}
+            //Uploader l'image dans un dossier
+            //Chemin complet de sauvegarde de l'image
+            //string filePath = @"C:\Users\ihab\source\repos\CoursMCPDNETF\coursAspNetMVC\wwwroot\avatar-" + nom +"-"+avatar.FileName;
+            string filePath = Path.Combine(_env.WebRootPath, "avatar","avatar-" + nom +"-"+avatar.FileName);
+
+            //Créer un flux pour sauvegarder l'image => A l'aide de la classe FILE
+            Stream stream = System.IO.File.Create(filePath);
+            avatar.CopyTo(stream);
+            stream.Close();
+            //Enregistrer dans la base de données
+            string chemin =  "avatar/avatar-" + nom + "-" + avatar.FileName;
+            Avatar a = new Avatar()
+            {
+                Chemin = chemin
+            };
+            a.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ coursAspNetMVC/Models/Avatar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coursAspNetMVC.Models
+{
+    public class Avatar
+    {
+        private int id;
+        private string chemin;
+        private static SqlConnection connection;
+
+        public int Id { get => id; set => id = value; }
+        public string Chemin { get => chemin; set => chemin = value; }
+
+
+
+        public bool Save()
+        {
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ihab\source\repos\CoursAP2019\basededonnees.mdf;Integrated Security=True;Connect Timeout=30");
+            string request = "INSERT INTO avatar (chemin) OUTPUT INSERTED.ID values(@chemin)";
+            SqlCommand command = new SqlCommand(request, connection);
+            command.Parameters.Add(new SqlParameter("@chemin", Chemin));
+            connection.Open();
+            Id = (int)command.ExecuteScalar();
+            command.Dispose();
+            connection.Close();
+            return Id > 0;
+        }
+
+        public static List<Avatar> GetAll()
+        {
+            List<Avatar> liste = new List<Avatar>();
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ihab\source\repos\CoursAP2019\basededonnees.mdf;Integrated Security=True;Connect Timeout=30");
+            string request = "select * from avatar";
+            SqlCommand command = new SqlCommand(request, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Avatar a = new Avatar()
+                {
+                    Id = reader.GetInt32(0),
+                    Chemin = reader.GetString(1)
+                };
+                liste.Add(a);
+            }
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+            return liste;
+        }
+    }
+}
